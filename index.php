3ed4d1d2449d4b590dedda204fb73ef5d01ca698
Abror <a_abror@mail.ru>
2016-11-18 19:35:31 +0200

    Added first time validation to index.php

--- index.php
+++ index.php
@@ -4,4 +4,13 @@
     ini_set('display_errors', 'On');
 ## END
 
-include_once('app/core/includes.php');
+/**
+ * Validation for first run
+ */
+if (!file_exists("app/config/config.xml")) {
+    echo "assa";
+    die();
+} else {
+    include_once('app/core/includes.php');
+}
+
